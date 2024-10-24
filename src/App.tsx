d020285b812dc076b106151f75d8c35dfa0113f2
maldonae <maldonadoeric69120@gmail.com>
2024-10-24 11:36:42 +0200

    Affichage du pokemon en props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
-function card() {
+function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
-export default card;
+export default App;
